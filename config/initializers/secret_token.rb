f68419cb849c071a6c62bedc433e09149bd6a513
thugrigby <plutonoodle.com>
2011-05-24 20:08:52 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'eff01f3ad73c0050362b15781cc96b591de80fa3629d75873d9ae717a52d53903b2924fba0c6732922bede92f77227b9cf3aec75618bc96df3d98d392b3e1218'
